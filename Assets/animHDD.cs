7d582747a64237da35584b0d62d2ba1e5a75a90f
Gabriel Ribeiro Dourado <dourado.gabrielribeirodourado@gmail.com>
2020-09-19 13:56:30 -0300

    update

---
+++ Assets/animHDD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animHDD : MonoBehaviour {
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < Input.touchCount; ++i){
+            if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("none")) {
+                anim.Play("Die");
+            }
+            else if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("DieRecover")) {
+                anim.Play("Die");
+            }
+            else if (Input.GetTouch(i).phase == TouchPhase.Began && anim.GetCurrentAnimatorStateInfo(0).IsName("Die")) {
+                anim.Play("DieRecover");
+            }
+        }
+
+    }
+}
---
+++ Assets/btnScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class btnScript : MonoBehaviour, IVirtualButtonEventHandler {
+
+    public GameObject btnObject;
+    public Animator Dog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnObject = GameObject.Find("VirtualButton");
+        Dog.GetComponent<Animator>();
+        btnObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Dog.Play("Die");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Dog.Play("DieRecover");
+    }
+
+}
